ba6bb12de4f5996af3bcc09cab6cb5e49c0cabb0
Malaka <malakasilva43@gmail.com>
2020-04-23 17:24:22 +0530

    main.dart replaced

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:infiveyears/animations/delayed_animation.dart';
-import 'package:infiveyears/UIs/login.dart';
+import 'package:infiveyears/login.dart';
 import 'package:rect_getter/rect_getter.dart';
 
-import '../animations/fade_out_anim.dart';
+import './animations/fade_out_anim.dart';
 
 void main() {
   runApp(MaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:infiveyears/UIs/main.dart';
+import 'package:infiveyears/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
